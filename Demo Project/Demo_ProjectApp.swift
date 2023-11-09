2c2b62003d98f5c40c1bda8168f43f41b2f51db7
Tejas <tejas@hookzapp.com>
2023-11-09 20:33:57 +0530

    Initial Commit

---
+++ Demo Project/Demo_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_ProjectApp.swift
+//  Demo Project
+//
+//  Created by Tejas Kashyap on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
