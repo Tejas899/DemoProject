e7fbcd1b92cc749dba26fb8f1b4a348e3186716b
Tejas <tejas@hookzapp.com>
2023-11-13 18:37:22 +0530

    code change for ios 16

---
+++ Demo Project/CustomeCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomeCell.swift
+//  Demo Project
+//
+//  Created by rajan kumar on 13/11/23.
+//
+
+import SwiftUI
+
+struct CustomeCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomeCell()
+}
